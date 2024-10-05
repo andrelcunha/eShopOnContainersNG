a4bbd013bd4dbac527c25b4b3738420abcc6bd6b
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2024-10-05 20:44:41 -0300

    Add Register page

--- src/WebApp/ALC.WebApp.MVC/Models/UserViewModel.cs
+++ src/WebApp/ALC.WebApp.MVC/Models/UserViewModel.cs
@@ -1,35 +1,50 @@
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ALC.WebApp.MVC.Models;
 
 public class UserLogin
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
+{
+    [Required(ErrorMessage = "The field {0} is required")]
+    [EmailAddress(ErrorMessage = "The field {0} is in a invalid format")]
+    public string Email { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "The field {0} is required")]
+    [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 6)]
+    public string Password { get; set; } = string.Empty;
+}
 
-    public class UserRegister
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public string ConfirmPassword { get; set; } = string.Empty;
-    }
+public class UserRegister
+{
+    [Required(ErrorMessage = "The field {0} is required")]
+    [EmailAddress(ErrorMessage = "The field {0} is in a invalid format")]
+    public string Email { get; set; } = string.Empty;
 
-    public class UserResponse
-    {
-        public string AccessToken { get; set; } = string.Empty;
-        public double ExpiresIn { get; set; }
-        public UserToken UserToken { get; set; } = new UserToken();
-    }
-    public class UserToken
-    {
-        public string Id { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public IEnumerable<UserClaim> Claims { get; set; } = new List<UserClaim>();
-    }
+    [Required(ErrorMessage = "The field {0} is required")]
+    [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 6)]
+    public string Password { get; set; } = string.Empty;
 
-    public class UserClaim
-    {
-        public string Value { get; set; } = string.Empty;
-        public string Type { get; set; } = string.Empty;
-    }
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    [DisplayName("Confirm your password")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
+
+public class UserResponse
+{
+    public string AccessToken { get; set; } = string.Empty;
+    public double ExpiresIn { get; set; }
+    public UserToken UserToken { get; set; } = new UserToken();
+}
+public class UserToken
+{
+    public string Id { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public IEnumerable<UserClaim> Claims { get; set; } = new List<UserClaim>();
+}
+
+public class UserClaim
+{
+    public string Value { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+}
