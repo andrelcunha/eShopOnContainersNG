8515909c318dc5acb1b0c74a2f761fa8e909ce00
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2024-10-07 00:03:17 -0300

    Add Models

---
+++ src/Services/ALC.Clients.API/Models/Address.cs
@@ -0,0 +1,31 @@
+using ALC.Core.DomainObjects;
+
+namespace ALC.Clients.API.Models;
+
+public class Address : Entity
+{
+    public string Street { get; private set; } = string.Empty;
+
+    public string Number { get; private set; } = string.Empty;
+    public string? AdditionalInfo { get; private set; }
+    public string Neighborhood { get; private set; } = string.Empty;
+    public string PostalCode { get; private set; } = string.Empty;
+    public string City { get; private set; } = string.Empty;
+    public string State { get; private set; } = string.Empty;
+    public string Country { get; private set; } = string.Empty;
+    public Guid ClientId { get; private set; }
+
+    public Client? Client { get; private set; }
+
+    public Address(string street, string number, string? additionalInfo, string neighborhood, string postalCode, string city, string state, string country)
+    {
+        Street = street;
+        Number = number;
+        AdditionalInfo = additionalInfo;
+        Neighborhood = neighborhood;
+        PostalCode = postalCode;
+        City = city;
+        State = state;
+        Country = country;
+    }    
+}
---
+++ src/Services/ALC.Clients.API/Models/Client.cs
@@ -0,0 +1,36 @@
+using System;
+using ALC.Core.DomainObjects;
+
+namespace ALC.Clients.API.Models;
+
+public class Client : Entity, IAggregationRoot
+{
+    public string Name { get; private set; } = string.Empty;
+    public Email Email { get; private set; }
+    public Cpf Cpf { get; private set; }
+    public bool Excluded { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+    public Address? Address { get; private set; }
+
+    public Client(Guid id,  string name, string email, string cpf)
+    {
+        Id = id;
+        Name = name;
+        Email = new Email(email);
+        Cpf = new Cpf(cpf);
+        Excluded = false;
+        CreatedAt = DateTime.Now;
+        UpdatedAt = DateTime.Now;
+    }
+
+    public void ChangeEmail(string email)
+    {
+        Email = new Email(email);
+    }
+
+    public void AddAddress(Address address)
+    {
+        Address = address;
+    }
+}
